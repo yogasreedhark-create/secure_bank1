60c9c0bcf394037754c1c951e74ea06a0df4adf6
yogasreedhark-create <yogasreedhark@gmail.com>
2025-12-12 00:24:28 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,9 @@
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "supersecretkey")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///bank.db")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

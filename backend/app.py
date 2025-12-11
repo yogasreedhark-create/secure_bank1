9a1d7b5be1193eb6380a85e155ea64acf029a67a
yogasreedhark-create <yogasreedhark@gmail.com>
2025-12-12 00:25:37 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
+from flask_cors import CORS
+from models import db
+from config import Config
+from routes import api
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db.init_app(app)
+CORS(app)
+
+app.register_blueprint(api, url_prefix="/api")
+
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
